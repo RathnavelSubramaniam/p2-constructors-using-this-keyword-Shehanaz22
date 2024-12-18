360b7701057f8356f3c658f3919574186bf8c92c
Shehanaz22 <shehanaz653@gmail.com>
2024-12-18 06:08:33 +0000

    2

--- Practical2.java
+++ Practical2.java
@@ -1,35 +1,35 @@
 class Product{
-  int ProductId;
-  String ProductName;
+  int productId;
+  String productName;
   double price;
   //Define instance variables:productId(int),productName(String),price(double)
   static int count ;
   Product()
   {
-    ProductName=null;
+    productName=null;
 
   }
   Product(int productId,String productName,double price)
   {
-    this.ProductId=productId;
-    this.ProductName=productName;
+    this.productId=productId;
+    this.productName=productName;
     this.price=price;
     count++;
 
   }
   //Static variable for product count
   //constructor to initialize product attributes
-  double discount(double price,double Discount)
+  double discount(double Price,double Discount)
   {
-    return((100-Discount)/100)*price;
+    return((100-Discount)/100)*Price;
 
   }
-  //Calculate and return the discounted prince 
+  //Calculate and return the discounted price 
   void displayProductInfo()
   {
-    System.out.println("Product Id:"+ ProductId);
-    System.out.println("Product Name:"+ ProductName);
-    System.out.println("Product Price:"+ price);
+    System.out.println("ProductId:"+productId);
+    System.out.println("ProductName:"+productName);
+    System.out.println("ProductPrice:"+price);
     //Display product information
     //Static method to retrieve total product count
   }
@@ -38,17 +38,23 @@ public class Practical2
 {
   public static void main(String[] args)
   {
-    Product product1=new Product(101, "Laptop", 999.99);
-    Product product2=new Product(202, "Smart Phone", 499.95);
-    Product product3=new Product(303, "Tablet", 299.50);
+    Product product1=new Product(101,"Laptop",999.99);
+    Product product2=new Product(202,"Smart Phone",499.95);
+    Product product3=new Product(303,"Tablet",299.50);
     Product calcDiscount=new Product();
 
+    //Create three Product objects with different data
     product1.displayProductInfo();
     product2.displayProductInfo();
     product3.displayProductInfo();
 
+    //Display product information
     double DiscountedPrice=calcDiscount.discount(product1.price,10);
-    System.out.println("Discounted Price :"+DiscountedPrice);
+    System.out.println("Discounted Price:"+DiscountedPrice);
     System.out.println(Product.count);
+
+    //Calculate and display discounted price
+    //Display total product count
   }  
 }
+
